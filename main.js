c5890e22c68c6cec2894e249b5358d549d9e50a7
shahzaibmalik55 <shahzaibmalik55@gmail.com>
2015-05-25 01:29:30 +0500

    Typescript Practice

---
+++ main.js
@@ -0,0 +1,17 @@
+//variable type is define by fist time we caonnot chnge the type
+var a = "asasas";
+alert(a);
+a = "asassa";
+//variable is define as number it can't be a string
+var num = 3;
+//    num = "shah";
+alert(num);
+//variable define as string it can't be a number
+var string = "shahzaib";
+//    string = 3; 
+//
+//variable can assign as a null 
+var nullVariable = "shahzaib";
+nullVariable = null;
+//given "any" attribute we can assign any type of value to a varaible
+var stringAndNumber = "shahzaib";
---
+++ main.ts
@@ -0,0 +1,21 @@
+//variable type is define by fist time we caonnot chnge the type
+var a : string = "asasas";
+   alert(a);
+   a = "asassa"
+
+//variable is define as number it can't be a string
+var num:Number = 3;
+//    num = "shah";
+alert(num);
+
+//variable define as string it can't be a number
+var string:string = "shahzaib";
+//    string = 3; 
+//
+//variable can assign as a null 
+var nullVariable = "shahzaib";
+    nullVariable = null;
+
+//given "any" attribute we can assign any type of value to a varaible
+var stringAndNumber:any = "shahzaib";
+
